2758e72077fa2e0145e2a3db1b9724399b344c3a
Jaehoon Son <son.jaehoon0826@gmail.com>
2026-01-08 21:47:52 -0500

    Update privacy, eula, and terms of service link

--- config.ts
+++ config.ts
@@ -3,6 +3,7 @@ import type { ExternalPathString } from "expo-router/build/typed-routes/types";
 interface AppMetadata {
   privacyPolicyUrl: ExternalPathString;
   endUserLicenseAgreementUrl: ExternalPathString;
+  termsOfServiceUrl: ExternalPathString;
 }
 
 export const BASE_API_ENDPOINT = __DEV__ ? "http://127.0.0.1:8000" : "http://109.199.116.115:8001" ;
@@ -11,7 +12,9 @@ export const BASE_API_ENDPOINT = __DEV__ ? "http://127.0.0.1:8000" : "http://109
 
 export const appMetadata: AppMetadata = {
   privacyPolicyUrl:
-    "https://theblucks.github.io/Elysia/privacy",
+    "https://brain-app-site.vercel.app/privacy",
   endUserLicenseAgreementUrl:
-    "https://theblucks.github.io/Elysia/eula",
+    "https://brain-app-site.vercel.app/eula",
+  termsOfServiceUrl:
+    "https://brain-app-site.vercel.app/terms",
 };
